782545fb0da78a99a95fd30260fcd66134cc987d
d.fathi <d.fathi@kian.digital>
2025-07-16 17:36:45 +0330

    feat(cach): Add redis conecting functionality

---
+++ src/data/cache/redis.go
@@ -0,0 +1,33 @@
+package cache
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/FathiMohammadDev/car-selling/config"
+	"github.com/redis/go-redis/v9"
+)
+
+var redisClient *redis.Client
+
+func InitRedis(cfg *config.Config) {
+	redisClient = redis.NewClient(&redis.Options{
+		Addr:         fmt.Sprintf("%s:%s", cfg.Redis.Host, cfg.Redis.Port),
+		Password:     cfg.Redis.Password,
+		DB:           0,
+		DialTimeout:  cfg.Redis.DialTimeout * time.Second,
+		ReadTimeout:  cfg.Redis.ReadTimeout * time.Second,
+		WriteTimeout: cfg.Redis.WriteTimeout * time.Second,
+		PoolSize:     cfg.Redis.PoolSize,
+		PoolTimeout:  cfg.Redis.PoolTimeout,
+	})
+
+}
+
+func GetRedis() *redis.Client {
+	return redisClient
+}
+
+func CloseRedis() {
+	redisClient.Close()
+}

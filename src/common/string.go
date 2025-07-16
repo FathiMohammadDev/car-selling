360516d3f6035b2b090579c1fd34e96f0b16e82e
d.fathi <d.fathi@kian.digital>
2025-07-16 11:39:33 +0330

    feat(common): Add password checker functionality

--- src/common/string.go
+++ src/common/string.go
@@ -3,6 +3,7 @@ package common
 import (
 	"math/rand"
 	"strings"
+	"unicode"
 
 	"github.com/FathiMohammadDev/car-selling/config"
 )
@@ -72,4 +73,64 @@ func GeneratePassword() string {
 	})
 	return string(inRune)
 
+}
+
+func CheckPassword(password string) bool {
+	cfg := config.GetConfig()
+	if len(password) < cfg.Password.MinLength {
+		return false
+	}
+
+	if cfg.Password.IncludeChars && !HasLetter(password) {
+		return false
+	}
+
+	if cfg.Password.IncludeDigits && !HasDigits(password) {
+		return false
+	}
+
+	if cfg.Password.IncludeLowercase && !HasLower(password) {
+		return false
+	}
+
+	if cfg.Password.IncludeUppercase && !HasUpper(password) {
+		return false
+	}
+
+	return true
+}
+func HasUpper(s string) bool {
+	for _, r := range s {
+		if unicode.IsUpper(r) && unicode.IsLetter(r) {
+			return true
+		}
+	}
+	return false
+}
+
+func HasLower(s string) bool {
+	for _, r := range s {
+		if unicode.IsLower(r) && unicode.IsLetter(r) {
+			return true
+		}
+	}
+	return false
+}
+
+func HasLetter(s string) bool {
+	for _, r := range s {
+		if unicode.IsLetter(r) {
+			return true
+		}
+	}
+	return false
+}
+
+func HasDigits(s string) bool {
+	for _, r := range s {
+		if unicode.IsDigit(r) {
+			return true
+		}
+	}
+	return false
 }

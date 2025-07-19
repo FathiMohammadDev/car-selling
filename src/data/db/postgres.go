555f887f9ebacc4bec6997a4318e4abd9525407a
d.fathi <d.fathi@kian.digital>
2025-07-19 11:02:40 +0330

    feat(data): Add postgres coneting functionality

---
+++ src/data/db/postgres.go
@@ -0,0 +1,50 @@
+package db
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/FathiMohammadDev/car-selling/config"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var dbClient *gorm.DB
+
+func InitDb(cfg *config.Config) error {
+	cnn := fmt.Sprintf("host=%s port=%s user=%s password=123456 dbname=%s sslmode=%s TimeZone=Asia/Tehran",
+		cfg.Postgres.Host, cfg.Postgres.Port, cfg.Postgres.User,
+		cfg.Postgres.DbName, cfg.Postgres.SSLMode)
+	var err error
+	dbClient, err = gorm.Open(postgres.Open(cnn), &gorm.Config{})
+
+	if err != nil {
+		return err
+	}
+
+	sqlDb, _ := dbClient.DB()
+
+	err = sqlDb.Ping()
+	if err != nil {
+		return err
+	}
+
+	sqlDb.SetMaxOpenConns(cfg.Postgres.MaxOpenConns)
+	sqlDb.SetMaxIdleConns(cfg.Postgres.MaxIdleConns)
+	sqlDb.SetConnMaxLifetime(cfg.Postgres.ConnMaxLifetime)
+
+	log.Println("DB conected successfuly.")
+
+	return nil
+
+}
+
+func GetDb() *gorm.DB {
+	return dbClient
+}
+
+func CloseDb() {
+	println("close DB")
+	con, _ := dbClient.DB()
+	con.Close()
+}

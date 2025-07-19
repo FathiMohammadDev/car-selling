4e7af0d6ec00d4c00702f8bc04aff09ef3cbc1aa
d.fathi <d.fathi@kian.digital>
2025-07-19 11:26:03 +0330

    feat: Exec postgres & server

--- src/cmd/main.go
+++ src/cmd/main.go
@@ -1,12 +1,29 @@
 package main
 
 import (
+	"log"
+
+	"github.com/FathiMohammadDev/car-selling/api"
 	"github.com/FathiMohammadDev/car-selling/config"
 	"github.com/FathiMohammadDev/car-selling/data/cache"
+	"github.com/FathiMohammadDev/car-selling/data/db"
 )
 
 func main() {
 	cfg := config.GetConfig()
+
 	defer cache.CloseRedis()
-	cache.InitRedis(cfg)
+	err := cache.InitRedis(cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer db.CloseDb()
+	err = db.InitDb(cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	api.InitServer(cfg)
+
 }

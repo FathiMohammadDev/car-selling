5f724a8a44c24aa6603212d39cb4ec58579f4079
m.fathi <srgcm4415@gmail.com>
2025-08-06 10:28:05 +0330

    feat: init gearbox  dto

--- src/api/dto/car.go
+++ src/api/dto/car.go
@@ -12,3 +12,16 @@ type CarTypeRes struct {
 	Id   int    `json:"id"`
 	Name string `json:"name"`
 }
+
+type CreateGearboxReq struct {
+	Name string `json:"name" binding:"required,alpha,min=3,max=15"`
+}
+
+type UpdateGearboxReq struct {
+	Name string `json:"name" binding:"required,alpha,min=3,max=15"`
+}
+
+type GearboxRes struct {
+	Id   int    `json:"id"`
+	Name string `json:"name"`
+}

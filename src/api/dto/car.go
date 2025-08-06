eeeb82a6d7738c79f33d79457f282752915f3048
m.fathi <srgcm4415@gmail.com>
2025-08-06 11:30:28 +0330

    feat: init car model dto

--- src/api/dto/car.go
+++ src/api/dto/car.go
@@ -25,3 +25,25 @@ type GearboxRes struct {
 	Id   int    `json:"id"`
 	Name string `json:"name"`
 }
+
+type CreateCarModelReq struct {
+	Name      string `json:"name" binding:"required,min=3,max=15"`
+	CompanyId int    `json:"companyId" binding:"required"`
+	CarTypeId int    `json:"carTypeId" binding:"required"`
+	GearboxId int    `json:"gearboxId" binding:"required"`
+}
+
+type UpdateCarModelReq struct {
+	Name      string `json:"name,omitempty"`
+	CompanyId int    `json:"companyId,omitempty"`
+	CarTypeId int    `json:"carTypeId,omitempty"`
+	GearboxId int    `json:"gearboxId,omitempty"`
+}
+
+type CarModelRes struct {
+	Id      int        `json:"id"`
+	Name    string     `json:"name"`
+	CarType CarTypeRes `json:"carType"`
+	Company CompanyRes `json:"company"`
+	Gearbox GearboxRes `json:"gearbox"`
+}
